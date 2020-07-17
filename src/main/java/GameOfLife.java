a247337d97c07e25c3d93808a586136463935b06
lamak <lamak.qaizar@careem.com>
2020-07-18 02:46:56 +0500

    r removed unused methods

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -73,32 +73,6 @@ public class GameOfLife {
         this.cells.set(row, column, DEAD_CELL);
     }
 
-    private int getNumberOfLivingNeighboursFor(int[][] cells, int row, int column) {
-        int livingNeighbours = 0;
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (isNeighbour(i, j) &&
-                        isWithinGrid(row, column, i, j)) {
-                    if (cells[row + i][column + j] == LIVING_CELL) {
-                        livingNeighbours++;
-                    }
-                }
-            }
-        }
-        return livingNeighbours;
-    }
-
-    private boolean isNeighbour(int i, int j) {
-        return i != 0 || j != 0;
-    }
-
-    private boolean isWithinGrid(int row, int column, int i, int j) {
-        return row + i >= 0
-                && column + j >= 0
-                && row + i < cells.getRows()
-                && column + j < cells.getColumns();
-    }
-
     public void assertState(int[][] cells) {
         assert Arrays.deepEquals(this.getCells(), cells);
     }
