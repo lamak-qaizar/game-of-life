c1ead448de060928b1f803a3938e92649695f117
lamak <lamak.qaizar@careem.com>
2020-07-18 00:24:40 +0500

    a extracted out bounds check into method

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -27,7 +27,7 @@ public class GameOfLife {
         int livingNeighbours = 0;
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                if (row + i >= 0 && column + j >= 0 && column + j < cells[row].length && row + i < cells.length) {
+                if (isWithinGrid(row, column, i, j)) {
                     if (cells[row + i][column + j] == 1) {
                         livingNeighbours++;
                     }
@@ -35,6 +35,13 @@ public class GameOfLife {
             }
         }
         return livingNeighbours;
+    }
+
+    private boolean isWithinGrid(int row, int column, int i, int j) {
+        return row + i >= 0
+                && column + j >= 0
+                && row + i < cells.length
+                && column + j < cells[row].length;
     }
 
     public void assertState(int[][] cells) {
