4d6c2a1afc651155d1b821508381f753a8e01cd4
lamak <lamak.qaizar@careem.com>
2020-07-17 23:29:51 +0500

    F added test for a lifeless board

---
+++ src/main/java/GameOfLife.java
@@ -0,0 +1,13 @@
+public class GameOfLife {
+
+    public GameOfLife(int width, int height) {
+    }
+
+    public void tick() {
+
+    }
+
+    public boolean hasLife() {
+        return false;
+    }
+}
--- src/test/java/GameOfLifeShould.java
+++ src/test/java/GameOfLifeShould.java
@@ -1,10 +1,14 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class GameOfLifeShould {
 
     @Test
-    public void doSomething() {
-        Assert.fail();
+    public void remainLifelessWithEmptyBoard() {
+        GameOfLife gameOfLife = new GameOfLife(4, 4);
+        gameOfLife.tick();
+        assertEquals(gameOfLife.hasLife(), false);
     }
 }
