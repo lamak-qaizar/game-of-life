98a195b0a59e71b9724effa52aec6f41167d4f26
lamak <lamak.qaizar@careem.com>
2020-07-22 14:23:50 +0500

    a extracted method in GridInputAdapter to reduce indentation

--- src/main/java/grid/GridInputAdapter.java
+++ src/main/java/grid/GridInputAdapter.java
@@ -7,9 +7,13 @@ public class GridInputAdapter extends Grid {
 
     public GridInputAdapter(int[][] cells) {
         for (int row = 0; row < cells.length; row++) {
-            for (int column = 0; column < cells[row].length; column++) {
-                set(new Coordinate(row, column), Cell.create(cells[row][column]));
-            }
+            createRow(cells[row], row);
+        }
+    }
+
+    private void createRow(int[] cells, int row) {
+        for (int column = 0; column < cells.length; column++) {
+            set(new Coordinate(row, column), Cell.create(cells[column]));
         }
     }
 }
